ed4640bb6f7f772e92b162bd5bd2f97fe5c675b3
Jyunee54 <jyunee54@gmail.com>
2025-11-20 17:20:47 +0900

    [feat/#15] BaseResponse 수정

--- app/src/main/java/sopt/org/starbucks/data/network/BaseResponse.kt
+++ app/src/main/java/sopt/org/starbucks/data/network/BaseResponse.kt
@@ -5,12 +5,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class BaseResponse<T>(
-    @SerialName("status")
-    val status: Int,
     @SerialName("code")
-    val code: String,
-    @SerialName("message")
-    val message: String,
+    val code: Int,
+    @SerialName("msg")
+    val msg: String,
     @SerialName("data")
     val data: T?
 )
