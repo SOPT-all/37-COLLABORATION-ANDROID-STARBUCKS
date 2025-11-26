0a29fc5a31c9eb2aaae881239184a9f08041269b
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-27 03:44:35 +0900

    [feat/#38] repository 추가

--- app/src/main/java/sopt/org/starbucks/data/repository/MyMenuRepository.kt
+++ app/src/main/java/sopt/org/starbucks/data/repository/MyMenuRepository.kt
@@ -1,9 +1,12 @@
 package sopt.org.starbucks.data.repository
 
 import sopt.org.starbucks.data.datasource.MyMenuDataSource
+import sopt.org.starbucks.data.mapper.toData
 import sopt.org.starbucks.data.mapper.toDomain
 import sopt.org.starbucks.data.model.MenuDetailModel
 import sopt.org.starbucks.data.model.MyMenu
+import sopt.org.starbucks.data.model.MyMenuOptionModel
+import sopt.org.starbucks.data.model.OptionItemModel
 import sopt.org.starbucks.data.network.handleApiResponse
 import sopt.org.starbucks.data.network.safeApiCall
 import javax.inject.Inject
@@ -11,25 +14,34 @@ import javax.inject.Singleton
 
 @Singleton
 class MyMenuRepository
-    @Inject
-    constructor(
-        private val myMenuDataSource: MyMenuDataSource
-    ) {
-        suspend fun getMyMenuList(): Result<List<MyMenu>> =
-            safeApiCall {
-                myMenuDataSource
-                    .getMyMenuList()
-                    .handleApiResponse()
-                    .getOrThrow()
-                    .toDomain()
-            }
+@Inject
+constructor(
+    private val myMenuDataSource: MyMenuDataSource
+) {
+    suspend fun getMyMenuList(): Result<List<MyMenu>> =
+        safeApiCall {
+            myMenuDataSource
+                .getMyMenuList()
+                .handleApiResponse()
+                .getOrThrow()
+                .toDomain()
+        }
 
-        suspend fun getMyMenuDetail(menuId: Long): Result<MenuDetailModel> =
-            safeApiCall {
-                myMenuDataSource
-                    .getMyMenuDetail(menuId)
-                    .handleApiResponse()
-                    .getOrThrow()
-                    .toDomain()
-            }
-    }
+    suspend fun getMyMenuDetail(menuId: Long): Result<MenuDetailModel> =
+        safeApiCall {
+            myMenuDataSource
+                .getMyMenuDetail(menuId)
+                .handleApiResponse()
+                .getOrThrow()
+                .toDomain()
+        }
+
+    suspend fun updateMyMenuOption(menuId: Long, request: OptionItemModel): Result<MyMenuOptionModel> =
+        safeApiCall {
+            myMenuDataSource
+                .updateMyMenuOption(menuId, request.toData())
+                .handleApiResponse()
+                .getOrThrow()
+                .toDomain()
+        }
+}
