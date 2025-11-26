957f5c9a9ba4d653ae6fd0c2dd3d305b0115ceb9
주아연 <142514626+znayeonzn@users.noreply.github.com>
2025-11-27 03:40:56 +0900

    [feat/#38] service 추가

--- app/src/main/java/sopt/org/starbucks/data/service/MyMenuService.kt
+++ app/src/main/java/sopt/org/starbucks/data/service/MyMenuService.kt
@@ -1,9 +1,13 @@
 package sopt.org.starbucks.data.service
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.Path
+import sopt.org.starbucks.data.dto.request.MyMenuOptionRequestDto
 import sopt.org.starbucks.data.dto.response.MyMenuDetailDto
 import sopt.org.starbucks.data.dto.response.MyMenuListDto
+import sopt.org.starbucks.data.dto.response.MyMenuOptionResponseDto
 import sopt.org.starbucks.data.network.BaseResponse
 
 interface MyMenuService {
@@ -14,4 +18,10 @@ interface MyMenuService {
     suspend fun getMyMenuDetail(
         @Path("menuId") menuId: Long
     ): BaseResponse<MyMenuDetailDto>
+
+    @PATCH("/api/v1/mymenu/{id}")
+    suspend fun updateMyMenuOption(
+        @Path("menuId") menuId: Long,
+        @Body requestDto: MyMenuOptionRequestDto
+    ): BaseResponse<MyMenuOptionResponseDto>
 }
